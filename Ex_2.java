5ff6ae55c678c2831c837c9e3a4baeacec96c60a
Raihan_Islam <iraihan705@gmail.com>
2025-08-25 20:38:33 +0600

    File change from Problem to Example

---
+++ Ex_2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.thebeginning;
+import java.util.*;
+/**
+ *
+ * @author USER
+ */
+
+public class Ex_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+        Scanner name = new Scanner(System.in);
+        System.out.println("Enter YOur Name :");
+        String n =  name.nextLine();
+        System.out.println("Hi!"+n);
+        
+    }
+}
